86a94808ccea67862482f291aa0aa0dba128ac26
Andreas Pohl <apohl79@gmail.com>
2012-02-04 21:27:59 +0000

    Changed module tags and enabled proguard

--- Android.mk
+++ Android.mk
@@ -1,15 +1,12 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := eng development
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := OMParts
 LOCAL_CERTIFICATE := platform
-
-# Required so that symbols used by the jni library doesn't get stripped out
-LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
 
