0a127ec553c7dd2e78db8873991f6126cd9690a7
Andreas Pohl <apohl79@gmail.com>
2012-11-30 19:02:42 +0100

    Clean up

--- src/com/osarmod/omparts/OMProperties.java
+++ src/com/osarmod/omparts/OMProperties.java
@@ -9,18 +9,17 @@ import android.util.Log;
 
 public class OMProperties {
 	private static final String TAG = "OMParts.OMProperties";
-	private static String SDCARD_SWITCH_FILE = "/data/local/switch_sdcard";
 
 	public static String getVersion(String def) {
 		return SystemProperties.get("ro.osarmod.version", def);
 	}
 
 	public static String getOSType() {
-		return SystemProperties.get("ro.osarmod.ostype", "cm7");
+		return SystemProperties.get("ro.osarmod.ostype", "unknown");
 	}
 
 	public static String getDevice() {
-		return SystemProperties.get("ro.osarmod.device", "galaxysmtd");
+		return SystemProperties.get("ro.osarmod.device", "unknown");
 	}
 
 	public static String getOsarmodType() {
@@ -28,40 +27,6 @@ public class OMProperties {
 	}
 
 	public static String getSdCard() {
-		String path;
-		if (getOSType().equals("cm10")) { // use build.prop value from cm10 on
-			path = SystemProperties.get("ro.osarmod.ota.download", "/mnt/sdcard");
-		} else {
-			path = "/mnt/sdcard";
-			if (SystemProperties.getInt("persist.sys.vold.switchexternal", 1) != 1) {
-				path = "/mnt/emmc";
-			}
-		}
-		return path;
-	}
-
-	public static boolean getSwitchSdCard() {
-		File f = new File(SDCARD_SWITCH_FILE);
-		return f.exists();
-	}
-
-	public static void setSwitchSdCard(boolean b) {
-		// we need root permissions now
-		Process p;
-		try {
-			p = Runtime.getRuntime().exec("su");
-			Log.v(TAG, "Got root access");
-			DataOutputStream out = new DataOutputStream(p.getOutputStream());
-			if (b) {
-				out.writeBytes("touch " + SDCARD_SWITCH_FILE + "\n");
-				Log.v(TAG, "Created " + SDCARD_SWITCH_FILE);
-			} else {
-				out.writeBytes("rm " + SDCARD_SWITCH_FILE + "\n");
-				Log.v(TAG, "Removed " + SDCARD_SWITCH_FILE);				
-			}
-			out.flush();
-		} catch (IOException e1) {
-			Log.e(TAG, "su failed: " + e1.getMessage());
-		}
+		return SystemProperties.get("ro.osarmod.ota.download", "/mnt/sdcard");
 	}
 }
